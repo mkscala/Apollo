3e1c76f7cba3ed6bc091ec8e75261a82a909ce15
enxebre <alberto.garcial@hotmail.com>
2015-06-24 15:07:27 +0100

    Run marathon as a docker container.

--- tests/spec/marathon/marathon_spec.rb
+++ tests/spec/marathon/marathon_spec.rb
@@ -1,9 +1,22 @@
 require 'spec_helper'
 
-describe package('marathon') do
-  it { should be_installed }
+describe docker_container('marathon') do
+  it { should be_running }
+  it { should have_volume('/tmp/docker.sock','/var/run/docker.sock') }
+  it { should have_volume('/store','/etc/marathon/store') }
+end
+
+describe port(8080) do
+  it { should be_listening }
+end
+
+describe command("/usr/local/bin/marathon-wait-for-listen.sh") do
+  its(:exit_status) { should eq 0 }
 end
 
 describe service('marathon') do
   it { should be_running.under('upstart') }
 end
+
+
+
